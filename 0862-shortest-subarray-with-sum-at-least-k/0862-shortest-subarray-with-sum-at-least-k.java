0c8403ca0c45650587cf59bc1cd0e0c2bcc3c5a2
Sumit Jaiswal <88160905+sumit2803s@users.noreply.github.com>
2024-11-17 14:00:41 +0530

    Time: 39 ms (44.73%), Space: 62.5 MB (27.49%) - LeetHub

---
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int shortestSubarray(int[] nums, int k) {
+         int n = nums.length;
+        long[] prefixSum = new long[n + 1];
+        
+        for (int i = 0; i < n; ++i) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        int minLength = Integer.MAX_VALUE;
+        
+        for (int i = 0; i <= n; ++i) {
+            while (!dq.isEmpty() && prefixSum[i] - prefixSum[dq.peekFirst()] >= k) {
+                minLength = Math.min(minLength, i - dq.pollFirst());
+            }
+            
+            while (!dq.isEmpty() && prefixSum[i] <= prefixSum[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            
+            dq.addLast(i);
+        }
+        
+        return (minLength == Integer.MAX_VALUE) ? -1 : minLength;
+    }
+}
